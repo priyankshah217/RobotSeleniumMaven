2997711aba66938e204f839ebb85ab6895509395
priyank <priyank.shah217@gmail.com>
2019-09-23 09:51:15 +0530

    Handling argument from command line, if its not provided then it will check in property file.

--- src/test/kotlin/io/automation/utils/DriverManager.kt
+++ src/test/kotlin/io/automation/utils/DriverManager.kt
@@ -3,6 +3,8 @@ package io.automation.utils
 
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.remote.RemoteWebDriver
 
 
 object DriverManager {
@@ -10,16 +12,36 @@ object DriverManager {
     private var driver: WebDriver? = null
 
     fun getDriver(): WebDriver? {
+        var browserName: String?
+
+        browserName = System.getProperty("browser.name") ?: null
+        if (browserName == null || browserName == "") {
+            val properties = PropertyReader.readPropertiesFromFile("automation-config.properties")
+            browserName = properties.getProperty("browser.name").toLowerCase()
+        }
         return when {
             driver == null -> {
-                driver = ChromeDriver()
+                driver = getDriverType(browserName)
                 driver
             }
-            (driver as ChromeDriver).sessionId == null -> {
-                driver = ChromeDriver()
+            (driver as RemoteWebDriver).sessionId == null -> {
+                driver = getDriverType(browserName)
                 driver
             }
             else -> driver
         }
+    }
+
+    private fun getDriverType(browserName: String?): RemoteWebDriver? {
+        val driver: RemoteWebDriver?
+        when {
+            browserName != "" -> driver = when (browserName) {
+                "firefox", "ff" -> FirefoxDriver()
+                "chrome" -> ChromeDriver()
+                else -> throw RuntimeException("Invalid browser type")
+            }
+            else -> throw RuntimeException("Invalid browser type")
+        }
+        return driver
     }
 }
